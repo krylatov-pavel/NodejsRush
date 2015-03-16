26de9e62c64476f2ed0fef98cea536e91bcf9874
krylatov-pavel <pavel.krylatov@softensity.com>
2015-03-16 11:33:57 +0300

    finished renamer;

--- 02_renamer/index.js
+++ 02_renamer/index.js
@@ -5,21 +5,24 @@ var argv = require('minimist')(process.argv.slice(2)),
     fs = require('fs'),
     pathModule = require('path');
 
-
 if (argv.dirname != undefined && argv.find != undefined && argv.replace != undefined){
-    var action = (function(find, replace){
+    var rename = (function(find, replace){
         return function(err, path){
-            var name = pathModule.basename(path);
-
+            var name = pathModule.basename(path).toLowerCase();
+            if (name.indexOf(find) > -1){
+                var newPath = [pathModule.dirname(path), name.replace(find, replace)].join("/");
+                fs.rename(path, newPath, null);
+            }
         }
     })(argv.find, argv.replace);
+    readDir(argv.dirname, rename);
 } else {
     console.log("argument/s is/are missing.");
 }
 
 function readDir(path, action){
     fs.readdir(path, function(err, list){
-       if (err != null){
+       if (err == null){
            list.forEach((function(file){
                var filePath = path + "/" + file;
                fs.stat(filePath, function(err, stat){

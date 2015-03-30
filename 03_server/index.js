269279906fc95433f67b68d448213571ea992a8b
Anton <antonio.narkevich@gmail.com>
2015-03-30 22:23:05 +0300

    Update index.js

--- 03_server/index.js
+++ 03_server/index.js
@@ -6,11 +6,13 @@ var http = require('http'),
     fs = require('fs');
 
 var server = http.createServer(function(request, response){
+    //path.join (Windows vs. Linux)
     var filePath = "." + request.url;
 
     fs.exists(filePath, function(exists){
        if (exists){
            fs.readFile(filePath, function(err, content){
+               //if (err)
                if (err == null){
                    var mimeType = mime.lookup(filePath);
 
@@ -22,6 +24,7 @@ var server = http.createServer(function(request, response){
                }
            })
 
+        //Else это зло if (smth) { return } идем дальше
        } else {
            response.writeHead(404, {"content-type" : "text/plain"});
            response.end("File not found", "utf-8");
@@ -29,4 +32,9 @@ var server = http.createServer(function(request, response){
     });
 });
 
+//Тут вторым параметром идет колбек. Можно console.log когда сервер фактически запустился
 server.listen(8080);
+
+//Этот сервер может отдать index.js т.е. свои же исохдники, если запросить. :) То есть подвержен exploit'у.
+//Но суть вообще в том, что для продакшна нужно использовать nginx или на худой конец express-static
+//Так как таких эксплоитов огромное количество и все самому реализовать это очень проблематично.
